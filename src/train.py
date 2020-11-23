aaa67fa5ab02868745cb4e085f0fcfad3133fa3b
rahul81 <rahuldas.rr81@gmail.com>
2020-11-23 21:11:27 +0530

    changed structure

--- src/train.py
+++ src/train.py
@@ -13,21 +13,16 @@ if __name__ == "__main__":
     df['target'] = df['airline_sentiment'].map({"positive":1,"negative":0})
     df.drop(["Unnamed: 0","airline_sentiment"],axis=1,inplace=True)
 
-    # for a in [.2,.3,.4,.5,.6,.8,.9,1,1.5,2]:
-    #     model = MultinomialNB(alpha=a)
-    #     SA = pipeline.Sentiment_Analysis()
-    #     SA.train_model(df,model)
-    #     print("**********************")
-    #     print("Model with Alpha = ",a)
-    #     print("**********************")
-
-    for a in [10,25,50,100,200,300,400,500]:
-        model = BaggingClassifier(MultinomialNB(alpha=.6), n_estimators=a)
+    for a in [.2,.3,.4,.5,.6,.8,.9,1,1.5,2]:
+        model = MultinomialNB(alpha=a)
         SA = pipeline.Sentiment_Analysis()
         SA.train_model(df,model)
         print("**********************")
-        print("Model with n_estimators = ",a)
+        print("Model with Alpha = ",a)
         print("**********************")
+
+    
+
 
 
 
